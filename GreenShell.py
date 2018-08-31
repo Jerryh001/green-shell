0ad370c6982031741751e05cfc71efa64c41b7c4
Jerryh001 <jerryh001@hotmail.com>
2018-08-31 11:19:06 +0800

    add logging

--- GreenShell.py
+++ GreenShell.py
@@ -1,13 +1,8 @@
 import discord
-import os
-import json
-import pytz
-import time
-import asyncio
-import importlib
-from datetime import datetime
-from datetime import timezone
 from discord.ext import commands
+import importlib
+import logging
+
 import kekekemonitor as km
 
 TOKEN="NDgzMjQxMTQzODM2OTk5Njkx.DmQmDg.9YVzFdm_zE3ulbKCN1YULe_phlA"
@@ -35,6 +30,30 @@ async def moniter(ctx,kchannel:str):
 async def hi(ctx):
     await ctx.send("Hello")
 
-bot.remove_command('help')
+@bot.command()
+async def cmd(ctx, *, cmd:str):
+    try:
+        ret=eval(cmd)
+        logging.info("eval({0}) successed ,return:\n{1}".format(cmd,ret))
+        await ctx.send("`{0}`".format(ret))
+    except:
+        logging.warning("eval({0}) failed".format(cmd))
+        await ctx.send("`eval({0})` failed".format(cmd))
 
-bot.run(TOKEN)
+@bot.command()
+async def loglevel(ctx, level:str,logger_name:str="" ):
+    try:
+        logger=logging.getLogger(logger_name)
+        level_old=logger.level
+        logger.setLevel(eval("logging."+level.upper()))
+        logging.info("logger {0} 's level changed from {1} to {0.level}({2})".format(logger,level_old,level.upper()))
+        await ctx.send("change success")
+    except:
+        logging.warning("change {0}'s level to {1} failed".format(logger,level))
+        await ctx.send("change failed")
+
+if __name__=="__main__":
+    logging.basicConfig(level=logging.INFO)
+    bot.remove_command('help')
+
+    bot.run(TOKEN)
