d6cdfe191bc240242e2341937b3d802137f6f286
Jerryh001 <jerryh001@hotmail.com>
2018-09-19 12:04:55 +0800

    login

--- kekeke/monitor.py
+++ kekeke/monitor.py
@@ -5,7 +5,6 @@ from .message import Message,MessageType
 class Monitor(object):
     _url = "https://kekeke.cc/com.liquable.hiroba.gwt.server.GWTHandler/squareService"
     _header = {"content-type": "text/x-gwt-rpc; charset=UTF-8"}
-    _payload = r"7|0|6|https://kekeke.cc/com.liquable.hiroba.square.gwt.SquareModule/|53263EDF7F9313FDD5BD38B49D3A7A77|com.liquable.hiroba.gwt.client.square.IGwtSquareService|getLeftMessages|com.liquable.gwt.transport.client.Destination/2061503238|/topic/{0}|1|2|3|4|1|5|5|6|"
     _log=logging.getLogger(__name__)
     
     def __init__(self,channel:str,stdout):
@@ -16,9 +15,10 @@ class Monitor(object):
         
 
     async def GetChannelHistoryMessages(self,start_from:datetime=None)->list:
+        _payload = r"7|0|6|https://kekeke.cc/com.liquable.hiroba.square.gwt.SquareModule/|53263EDF7F9313FDD5BD38B49D3A7A77|com.liquable.hiroba.gwt.client.square.IGwtSquareService|getLeftMessages|com.liquable.gwt.transport.client.Destination/2061503238|/topic/{0}|1|2|3|4|1|5|5|6|"
         ans=list()
         try:
-            async with aiohttp.request("POST",self._url, data=self._payload.format(self.channel),headers=self._header) as r:
+            async with aiohttp.request("POST",self._url, data=_payload.format(self.channel),headers=self._header) as r:
                 resp = await r.text()
         except:
             self._log.error("Fetch history messages from channel "+self.channel+" failed")
@@ -87,6 +87,13 @@ class Monitor(object):
             await asyncio.sleep(period)
         self._log.info("stopped "+self.channel+" moniter")
 
+    async def GetToken(self):
+        _payload = r"7|0|7|https://kekeke.cc/com.liquable.hiroba.square.gwt.SquareModule/|53263EDF7F9313FDD5BD38B49D3A7A77|com.liquable.hiroba.gwt.client.square.IGwtSquareService|startSquare|com.liquable.hiroba.gwt.client.square.StartSquareRequest/2186526774|com.liquable.gwt.transport.client.Destination/2061503238|/topic/{0}|1|2|3|4|1|5|5|0|0|6|7|"
+        async with aiohttp.request("POST",self._url, data=_payload.format(self.channel),headers=self._header) as r:
+            resp = await r.text()
+        if resp[:4]==r"//OK":
+            token=json.loads(resp[4:])[-3][2]
+            return token
     async def Oversee(self):
         self._log.info("開始監視 "+self.channel)
         self._last_time=await self.GetLastMessageTime()
@@ -98,8 +105,11 @@ class Monitor(object):
             self._log.info("更新了 "+self.channel+" 的 "+str(len(data))+" 條訊息")
         self._log.info("開始常駐監聽 "+self.channel)
         SUBSCRIBE=r"""SUBSCRIBE
-        destination:/topic/{0}""".format(self.channel)
+destination:/topic/{0}""".format(self.channel)
+        LOGIN=r"""CONNECT
+login:{{"accessToken":"{0}", "nickname":"DiscordBot"}}""".format(await self.GetToken())
         async with websockets.connect("wss://ws.kekeke.cc/com.liquable.hiroba.websocket") as ws:
+            await ws.send(LOGIN)
             await ws.send(SUBSCRIBE)
             while not ws.closed:
                 try:
